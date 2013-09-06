e8f545d05754e7d166e6e95a232161a341270a8e
Tony Rogvall <tony@rogvall.se>
2013-09-06 20:06:12 +0200

    figured out the new formta for set_leds

--- src/edrone_motor.erl
+++ src/edrone_motor.erl
@@ -43,7 +43,11 @@ init() ->
 
     [ begin
 	  gpio:set_direction(M, in),
-	  {ok,<<16#E0,16#00>>} = command(U,16#e0,2),
+	  case command(U,16#e0,2) of
+	      {ok,<<16#E0,16#00>>} -> ok;
+	      {ok,Ret} ->
+		  io:format("motor%d cmd=~w reply=~w\n", [Mi,Ret])
+	  end,
 	  command(U, Mi, 1),
 	  gpio:set_direction(M, high)
       end || {Mi,M} <- [{1,?GPIO_M1},{2,?GPIO_M2},{3,?GPIO_M3},{4,?GPIO_M4}]],
@@ -66,8 +70,12 @@ command(U, Cmd, ReplyLen) ->
 set_pwm(U, Pwm0, Pwm1, Pwm2, Pwm3) ->
     uart:send(U, <<2#001:3, Pwm0:9, Pwm1:9, Pwm2:9, Pwm3:9, 0:1>>).
 
-set_leds(U, Led0, Led1, Led2, Led3) ->	      
-    uart:send(U, <<2#011:3, Led0:2, Led1:2, Led2:2, Led3:2, 0:5>>).
+set_leds(U, Led0, Led1, Led2, Led3) ->
+    uart:send(U, <<2#011:3, 
+		   Led0:1, Led1:1, Led2:1, Led3:1, 0:1,
+		   2#000:3,
+		   (Led0 bsr 1):1, (Led1 bsr 1):1, 
+		   (Led2 bsr 1):1, (Led3 bsr 1):1, 0:1 >>).
 
 run(U, M0, M1, M2, M3) ->
     V0 = min(max(0.0, M0), 1.0),
@@ -81,9 +89,3 @@ run(U, M0, M1, M2, M3) ->
     PWM3 = trunc(?PWM_MIN + V3*(?PWM_MAX - ?PWM_MIN)),
     set_pwm(U, PWM0, PWM1, PWM2, PWM3),
     {V0,V1,V2,V3}.
-
-
-
-
-		
-		
